06931483524ac9b7fc88ac46ad528049773d267e
Ponss17 <jeancastrogudiel@gmail.com>
2026-01-26 12:21:36 -0600

    Pruebas

--- public/js/dashboard/clips.js
+++ public/js/dashboard/clips.js
@@ -64,6 +64,9 @@ export const ClipsModule = {
                 UI.showToast('Actualizando clips...', 'info');
             }
 
+            console.log('[Clips] Session:', this.session);
+            console.log('[Clips] Fetching URL:', url);
+
             const response = await fetch(url, { headers });
 
             if (!response.ok) {
@@ -72,7 +75,9 @@ export const ClipsModule = {
             }
 
             const data = await response.json();
-            const clips = data.clips || [];
+            console.log('[Clips] API Response:', data);
+
+            const clips = Array.isArray(data) ? data : (data.clips || data.data || []);
 
             cache.set(cacheKey, clips, CACHE_TTL.CLIPS);
 
