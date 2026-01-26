b77923b767abb6244ccf61ff700a2c1d6893f9ca
Ponss17 <jeancastrogudiel@gmail.com>
2026-01-25 23:49:03 -0600

    feat: añadir utilidades frontend (Logger, APICache, Skeleton)
    Mejoras 3-5/8 - Frontend Performance:
    - Logger: wrapper para console (solo logs en localhost)
    - APICache: caché de requests HTTP con TTL
    - Skeleton: loaders para mejor UX
    
    Sin cambios a rutas existentes

---
+++ public/js/utils/core/logger.js
@@ -0,0 +1,15 @@
+export const Logger = {
+    log(...args) {
+        if (typeof window !== 'undefined' && window.location.hostname === 'localhost') {
+            console.log(...args);
+        }
+    },
+
+    warn(...args) {
+        console.warn(...args);
+    },
+
+    error(...args) {
+        console.error(...args);
+    }
+};
