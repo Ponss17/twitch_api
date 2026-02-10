44b66c3ff714490e1bc8f1c16d0da0a60cc7905d
Ponss17 <jeancastrogudiel@gmail.com>
2026-02-10 15:15:46 -0600

    Ajustes de login admin

--- src/config/env.ts
+++ src/config/env.ts
@@ -18,7 +18,8 @@ export const CONFIG = {
     TWITCH_CLIENT_ID: TWITCH_CLIENT_ID,
     TWITCH_CLIENT_SECRET: TWITCH_CLIENT_SECRET,
     TWITCH_REDIRECT_URI:
-        process.env.TWITCH_REDIRECT_URI || 'https://losperris.site/auth/twitch/callback',
+        process.env.TWITCH_REDIRECT_URI ||
+        'https://www.losperris.site/api/twitch/auth/twitch/callback',
     NODE_ENV: process.env.NODE_ENV || 'development',
     MAX_MESSAGE_TOKENS: 100,
     DISCORD_FEEDBACK_WEBHOOK_URL: process.env.DISCORD_FEEDBACK_WEBHOOK_URL,
